935318482b47482a60e6e3e8144da06e991289da
Alex Zhou <cosr330@gmail.com>
2024-03-12 15:45:20 -0700

    sss

--- Lab6.c
+++ Lab6.c
@@ -5,13 +5,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
-//#include <SoftwareSerial.h>
 #include "usart.h"
 #include "lcd.h"
+#include "SoftwareUart.h"
 
+// #define RED (1<<PC5)
+// #define YELLOW (1<<PC4)
+// #define RF_SET (1<<PC2)
+#define RED "PC3"
+#define YELLOW "PC4"
+#define BUFSIZE 80
 
 
 
@@ -35,7 +42,6 @@
                            -------
 */
 
-#define RF_SET P2_5
 
 unsigned int cnt = 0;
 volatile unsigned int reload;
@@ -44,8 +50,9 @@ ISR(TIMER1_COMPA_vect)
 {
 	OCR1A = OCR1A + reload;
 	PORTB ^= 0b00000011; // Toggle PB0 and PB1
+	//PORTD ^= 0b11000000;
+	//		   76543210
 }
-
 
 void wait_1ms(void)
 {
@@ -55,8 +62,6 @@ void wait_1ms(void)
 	
 	while((TCNT1-saved_TCNT1)<(F_CPU/1000L)); // Wait for 1 ms to pass
 }
-
-
 
 void waitms(int ms)
 {
@@ -104,12 +109,77 @@ long int GetPeriod (int n)
 	return overflow*0x10000L+(saved_TCNT1b-saved_TCNT1a);
 }
 
-// void RF_init(void){
-// 	// RX = 2, TX = 3
-// 	SoftwareSerial mySerial(23, 24);
-//   	// Set up the software serial port for RF communication
-//   	mySerial.begin(9600);
-// }
+// Toglle pins for me so i dont have to do it the long shitty way
+void setPin(char* pinName, int val) {
+    if(val != 0 && val != 1) { // check input validity
+        return;
+    }
+    // get port and pin from pin name
+    if(pinName[0] == 'P') {
+        char port = pinName[1];
+        int pin = pinName[2] - '0'; // covert ascii to int
+        // set ddr (data direction register)
+        switch (port) {
+            case 'B':
+                DDRB |= (1 << pin);
+                if(val == 1)
+                    PORTB |= (1 << pin); // set portb high (1)
+                else
+                    PORTB &= ~(1 << pin); // set portb low (0)
+                break;
+            case 'C':
+                DDRC |= (1 << pin);
+                if(val == 1)
+                    PORTC |= (1 << pin);
+                else
+                    PORTC &= ~(1 << pin);
+                break;
+            case 'D':
+                DDRD |= (1 << pin);
+                if(val == 1)
+                    PORTD |= (1 << pin);
+                else
+                    PORTD &= ~(1 << pin);
+                break;
+            default:
+                break; 
+        }
+    }
+}
+
+void SendATCommand (char * s)
+{	
+	char buffer[BUFSIZE];
+	printf("Command: %s", s);
+	PORTC &= ~(1<<2); // 'set' pin to 0 is 'AT' mode.
+	
+	waitms(5);
+	printf("1");
+	SendString(s);
+	printf("2");
+	GetString(buffer, BUFSIZE-1);
+	printf("3");
+	waitms(10);
+	printf("4");
+	PORTC |= (1<<2); // 'set' pin to 1 is normal operation mode.
+	printf("5");
+	printf("Response: %s\r\n", buffer);
+	printf("6");
+}
+
+void Init_RF(void){
+	// We should select an unique device ID.  The device ID can be a hex
+	// number from 0x0000 to 0xFFFF.  In this case is set to 0xABBA
+	SendATCommand("AT+DVID1357\r\n");  
+	// To check configuration
+	SendATCommand("AT+VER\r\n");
+	SendATCommand("AT+BAUD\r\n");
+	SendATCommand("AT+RFID\r\n");
+	SendATCommand("AT+DVID\r\n");
+	SendATCommand("AT+RFC\r\n");
+	SendATCommand("AT+POWE\r\n");
+	SendATCommand("AT+CLSS\r\n");
+}
 
 
 int main(void)
@@ -117,22 +187,26 @@ int main(void)
 	long int count;
 	float T, f, C, CuF;
 	unsigned int adc;
-	char buff[32];
+	char buff[BUFSIZE];
 	unsigned long newF;
-	
+	//char flag = 0;
 	reload=OCR1_RELOAD; // Reload value for default output frequency 
-
+	//bit: 76543210 // 1 = output for DDRn
 	DDRB=0b00000011; // PB1 (pin 15) and PB0 (pin 14) are our outputs
+	DDRC=0b00011000; // Creg outputs for LEDS
+	DDRD |= 0b00010000; // set DDRD[4] to 1 output for "SET" for JDY-40
 	PORTB |= 0x01; // PB0=NOT(PB1)
 	TCCR1B |= _BV(CS10);   // set prescaler to Clock/1
 	TIMSK1 |= _BV(OCIE1A); // output compare match interrupt for register A
-	
+	usart_init();
 	sei(); // enable global interupt
-	usart_init (); // configure the usart and baudrate
-	adc_init();
 	
+	
+	//bit:     76543210
 	DDRB  &= 0b11111101; // Configure PB1 as input
 	PORTB |= 0b00000010; // Activate pull-up in PB1
+	
+	ConfigureSoftwareUART(); // configure software UART for RF module
 
 	// Turn on timer with no prescaler on the clock.  We use it for delays and to measure period.
 	TCCR1B |= _BV(CS10); // Check page 110 of ATmega328P datasheet
@@ -140,25 +214,24 @@ int main(void)
 	waitms(500); // Wait for putty to start
 	printf("Period measurement using the free running counter of timer 1.\n"
 	       "Connect signal to PB1 (pin 15).\n");
-
-	flag = 0;
+	Init_RF();
 	
 	while (1)
 	{
-		printf("Frequency: ");
-    	usart_gets(buff, sizeof(buff)-1);
-    	newF=atol(buff);
+		// printf("Frequency: ");
+    	// usart_gets(buff, sizeof(buff)-1);
+    	// newF=atol(buff);
 
-	    if(newF>111000L)
-	    {
-	       printf("\r\nWarning: The maximum frequency that can be generated is around 111000Hz.\r\n");
-	       newF=111000L;
-	    }
-	    if(newF>0)
-	    {
-			reload=(F_CPU/(2L*newF))+1;  
-		    printf("\r\nFrequency set to: %ld\r\n", F_CPU/((reload-1)*2L));
-        }
+	    // if(newF>111000L)
+	    // {
+	    //    printf("\r\nWarning: The maximum frequency that can be generated is around 111000Hz.\r\n");
+	    //    newF=111000L;
+	    // }
+	    // if(newF>0)
+	    // {
+		// 	reload=(F_CPU/(2L*newF))+1;  
+		//     printf("\r\nFrequency set to: %ld\r\n", F_CPU/((reload-1)*2L));
+        // }
 		count=GetPeriod(100);
 		if(count>0)
 		{
@@ -171,27 +244,38 @@ int main(void)
 
 			printf("\033[A");
 			printf("\033[A");
-			
 
-			if(flag == 0){
-				LCDprint("Capacitance(uF)   ", 1, 1);
-				sprintf(buffer, "%guF", C);
-				LCDprint(buffer, 2, 1);
-			} else if(flag == 1){
-				LCDprint("Frequency(Hz)   ", 1, 1);
-				sprintf(buffer, "%luHz", f);
-				LCDprint(buffer, 2, 1);		
-			} else if(flag == 2){
-				LCDprint("Period(s)   ", 1, 1);
-				sprintf(buffer, "%fS", T);
-				LCDprint(buffer, 2, 1);		
-			}
+			// if(flag == 0){
+			// 	LCDprint("Capacitance(uF)   ", 1, 1);
+			// 	sprintf(buff, "%guF", C);
+			// 	LCDprint(buff, 2, 1);
+			// } else if(flag == 1){
+			// 	LCDprint("Frequency(Hz)   ", 1, 1);
+			// 	sprintf(buff, "%luHz", f);
+			// 	LCDprint(buff, 2, 1);		
+			// } else if(flag == 2){
+			// 	LCDprint("Period(s)   ", 1, 1);
+			// 	sprintf(buff, "%fS", T);
+			// 	LCDprint(buff, 2, 1);		
+			// }
 
 		}
 		else
 		{
 			printf("NO SIGNAL                     \r");
 		}
+
+		//sprintf(buff,"f=%fHz", f);
+		strcpy(buff, "test");
+		SendString(buff);
+		//setPin(RED,0); // red on
+		//setPin(YELLOW,1); //yellow off
+		//PORTC |= 0x03;
+		//PORTC &= ~(1<<0x03);
+		//PORTC &= ~(1<<0x04);
+		//PORTC |= (1<<0x04);
+		
+		
 		waitms(200);
 	}
 }
--- Lab6.mk
+++ Lab6.mk
@@ -1,12 +1,22 @@
 SHELL=cmd
-PORTN=$(shell type COMPORT.inc)
-OBJS=Lab6.o usart.o
+CC=c51
+COMPORT=$(shell type COMPORT.inc)
+COMPORTc=$(shell type COMPORTc.inc)
+OBJS=Lab6.o usart.o lcd.o SoftwareUart.o 
+OBJSc=Controller.obj
 CPU=-mmcu=atmega328p
 
 Lab6.elf: $(OBJS)
 	avr-gcc $(CPU) $(OBJS) -Wl,-Map,Lab6.map -Wl,-u,vfprintf -lprintf_flt -lm -o Lab6.elf
 	avr-objcopy -j .text -j .data -O ihex Lab6.elf Lab6.hex
 	@echo done!
+
+Controller.hex: $(OBJSc)
+	$(CC) $(OBJSc)
+	@echo Done!
+	
+Controller.obj: Controller.c
+	$(CC) -c Controller.c
 	
 Lab6.o: Lab6.c
 	avr-gcc --param=min-pagesize=0 -g -Os -mmcu=atmega328 -c Lab6.c
@@ -17,18 +27,45 @@ usart.o: usart.c usart.h
 lcd.o: lcd.c usart.h LCD.h
 	avr-gcc --param=min-pagesize=0 -g -Os -Wall -mmcu=atmega328p -c lcd.c
 
+SoftwareUart.o: SoftwareUart.c SoftwareUart.h
+	avr-gcc --param=min-pagesize=0 -g -Os -Wall -mmcu=atmega328p -c SoftwareUart.c
+
 clean:
-	@del *.hex *.elf *.o 2>nul
+	@del *.asm *.lkr *.lst *.map *.map *.o 2>nul
 
-FlashLoad:
+Flash_Device:
 	@taskkill /f /im putty.exe /t /fi "status eq running" > NUL
-	spi_atmega -CRYSTAL -p -v Lab6.hex
-	cmd /c start putty.exe -serial $(PORTN) -sercfg 115200,8,n,1,N
+	spi_atmega -CRYSTAL -p -v -d1 Lab6.hex
+	cmd /c start putty.exe -serial $(COMPORT) -sercfg 115200,8,n,1,N
 
-putty:
+Flash_Controller:
+	@Taskkill /IM putty.exe /F 2>NUL | wait 500
+	EFM8_prog.exe -ft230 -r -d0 Controller.hex
+	cmd /c start putty -serial $(COMPORTc) -sercfg 115200,8,n,1,N
+
+Flash_ALL:
+	@Taskkill /IM putty.exe /F 2>NUL | wait 500 
+	EFM8_prog.exe -ft230 -r -d0 Controller.hex
+	move COMPORT.inc COMPORTc.inc
+	spi_atmega -CRYSTAL -p -v -d1 Lab6.hex
+	cmd /c start putty -serial $(COMPORTc) -sercfg 115200,8,n,1,N
+	cmd /c start putty.exe -serial $(COMPORT) -sercfg 115200,8,n,1,N
+
+putty_Device:
 	@taskkill /f /im putty.exe /t /fi "status eq running" > NUL
-	cmd /c start putty.exe -serial $(PORTN) -sercfg 115200,8,n,1,N
+	cmd /c start putty.exe -serial $(COMPORT) -sercfg 115200,8,n,1,N
 
-dummy: Lab6.hex Lab6.map
+putty_Controller:
+	@Taskkill /IM putty.exe /F 2>NUL | wait 500
+	cmd /c start putty -serial $(COMPORTc) -sercfg 115200,8,n,1,N
+
+putty_ALL:
+	@Taskkill /IM putty.exe /F 2>NUL | wait 500
+	cmd /c start putty -serial $(COMPORTc) -sercfg 115200,8,n,1,N
+	cmd /c start putty -serial $(COMPORT) -sercfg 115200,8,n,1,N
+
+dummy: Lab6.hex Lab6.map Controller.hex Controller.map
 	@echo Hello from dummy!
-	
+
+explorer:
+	cmd /c start explorer .
